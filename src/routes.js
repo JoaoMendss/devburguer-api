1260a0f40ba1d86d026056e0fa965ac37a38765b
João Mendes <152667520+JoaoMendss@users.noreply.github.com>
2025-08-15 20:34:48 -0300

    arquivos iniciais do devburguer-api

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+
+const routes = new Router()
+
+routes.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Welcome to DevBurger API!' })
+})
+
+module.exports = routes
+// This file defines the routes for the DevBurger API. It currently has a single route that
