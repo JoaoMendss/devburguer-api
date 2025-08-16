9d77762a417b75d22a7d4e5c3c496ee610ccded7
JoaoMendss <joaoluiizmendes@gmail.com>
2025-08-15 21:23:27 -0300

    salvando alterações para continuar amanhã

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-const { Router } = require('express')
+import { Router } from "express";
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Welcome to DevBurger API!' })
-})
+routes.get("/", (req, res) => {
+	return res.status(200).json({ message: "Welcome to DevBurger API!" });
+});
 
-module.exports = routes
+export default routes;
 // This file defines the routes for the DevBurger API. It currently has a single route that
